3ebd9e76087164d1d259d1e278401eddba249101
SV <scottyv6@gmail.com>
2021-10-19 16:31:12 +0800

    Created workout model and initiated api.js file

---
+++ models/workout.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    required: 'A Date is required',
+    default: Date.now()
+  },
+  exercises:  {
+    type: [exerciseSchema]
+  }
+});
+
+const exerciseSchema = new Schema({
+  type: {
+    type: String,
+    required: "Enter exercise type",
+    trim: true
+  },
+  name: {
+    type: String,
+    required: "Enter exercise name",
+    trim: true
+  },
+  duration: {
+    type: Number,
+    required: "Enter duration of exercise"
+  },
+  weight: {
+    type: Number,
+    required: "Enter weight"
+  },
+  reps: {
+    type: Number,
+    required: "Enter number of repertitions"
+  },
+  sets: {
+    type: Number,
+    required: "Enter number of sets"
+  }
+})
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
