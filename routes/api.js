5765942e9fb8e9e14edf1a35e6262c0f8a898940
SV <scottyv6@gmail.com>
2021-10-24 22:18:47 +0800

    modified routes/web.js, added routes to routes/api.js and added readme

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,82 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js")
 
+//Get data from most recent workout
+router.get("/api/workouts", (req,res) => {
+    Workout.aggregate( [
+        {
+            $addFields: {
+                totalDuration: { $sum: "$exercises.duration"}
+            }
+        }
+    ])
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    }) 
+    .catch(err => {
+        res.status(400).jason(err);
+    })
+})
+
+
+
+
+//Add new exercise to workout plan
+router.put("/api/workouts/:id", (req,res) => {
+    Workout.findByIdAndUpdate(req.params.id, 
+        {
+            $push: {"exercises": req.body}
+        }
+        
+    )
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    }) 
+    .catch(err => {
+        res.status(400).jason(err);
+    })
+})
+
+
+
+
+
+//get data from last 7 days
+router.get("/api/workouts/range", (req,res) => {
+    Workout.aggregate( [
+        {
+            $sort: {day: -1}
+        },
+        {
+            $limit: 7
+        },
+        {
+            $addFields: {
+                totalDuration: { $sum: "$exercises.duration"}
+            }
+        }
+    ])
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    }) 
+    .catch(err => {
+        res.status(400).jason(err);
+    })
+})
+
+
+
+
+//Add new workout
+router.post("/api/workouts", (req,res) => {
+    Workout.create({} )
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    }) 
+    .catch(err => {
+        res.status(400).jason(err);
+    })
+})
+
+
+module.exports = router;
--- routes/web.js
+++ routes/web.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 const path = require("path");
 
 router.get("/exercise", function (req, res) {
-    res.sendFiles(path.join(_dirname, "../public/exercise.html"));
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
 router.get("/stats", function (req, res) {
-    res.sendFiles(path.join(_dirname, "../public/stats.html"));
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
+
+module.exports = router;  
